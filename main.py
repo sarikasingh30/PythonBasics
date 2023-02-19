d272b0ed04a0a1a0374a73376d32160dc2076a19
sarikasingh30 <sarika.goooogle.1330@gmail.com>
2023-02-19 21:59:51 +0530

    updated strings file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # --------------------------------------------------------------------------------------------------------
 # Print value upto to 2 decimal places.
+import textwrap
 t = 40
 print('{:.2f}'.format(t))
 # --------------------------------------------------------------------------------------------------------
@@ -9,4 +10,17 @@ b = tuple()
 d = dict()
 s = str()
 print(a, b, d, s)
+# --------------------------------------------------------------------------------------------------------
+# Wrap the string into a paragraph of width .
+
+
+def wrap(string, max_width):
+    y = textwrap.fill(string, max_width)
+    return y
+
+
+string = "ABCDEFGHIJKLIMNOQRSTUVWXYZ"
+max_width = 4
+x = wrap(string, max_width)
+print(x)
 # --------------------------------------------------------------------------------------------------------
--- strings.py
+++ strings.py
@@ -9,5 +9,24 @@ def fun(line):
 
 x = "Today is a beautiful day"
 ans = fun(x)
-print(ans)
+print(x, "=>", ans)
+# --------------------------------------------------------------------------------------------------------
+'''
+Sample Input =>  chris alan
+Sample Output => Chris Alan
+'''
+
+
+def solve(s):
+    x = s.split(" ")
+    c = []
+    for i in x:
+        c.append(i.capitalize())
+    ans = " ".join(c)
+    return ans
+
+
+s = "chris alan"
+ans = solve(s)
+print(s, "=>", ans)
 # --------------------------------------------------------------------------------------------------------
